d610a3f94a5c09cae72c85c0bce872dcbd5fffdf
gabrierabelo <gabriel.almeida97@gmail.com>
2020-01-30 15:48:50 -0300

    first commit

---
+++ src/test/kotlin/com/rabelo/movies/MoviesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rabelo.movies
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
