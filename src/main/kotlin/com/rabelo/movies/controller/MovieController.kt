fa1855c49adf4995505214478c6ad4d39a30bab1
gabrierabelo <gabriel.almeida97@gmail.com>
2020-01-31 10:03:52 -0300

    cria controller

---
+++ src/main/kotlin/com/rabelo/movies/controller/MovieController.kt
@@ -0,0 +1,23 @@
+package com.rabelo.movies.controller
+
+import com.rabelo.movies.model.Movie
+import com.rabelo.movies.repository.MovieRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Mono
+import java.util.*
+
+@RestController
+@RequestMapping(value = ["/movie"])
+class MovieEndpoint @Autowired constructor(private val repository: MovieRepository) {
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    fun post(@RequestBody movie: Movie): Mono<Movie> {
+        movie.id = UUID.randomUUID().toString()
+
+        return repository.save(movie)
+    }
+
+}
